9cb206c34bd1fb449b2cf6067ee4aa2a2f52e115
jim spring <jmspring@gmail.com>
2016-12-08 15:35:14 -0800

    simplify worker

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
 FROM mhart/alpine-node:latest
-# FROM mhart/alpine-node:6
 
 WORKDIR /src
 ADD . .
 
 # If you have native dependencies, you'll need extra tools
 # RUN apk add --no-cache make gcc g++ python
-RUN apk add --no-cache bash python gcc g++ make && \
-	mkdir config
-
-# If you need npm, don't use a base tag
-RUN npm install
+RUN apk add --no-cache --virtual .build-deps \
+	build-base python gcc g++ make \
+	&& npm install \
+	&& apk del .build-deps \
+	&& apk add --no-cache bash \
+	&& mkdir config
 
 EXPOSE 5000
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-var path = require('path')
-var express = require('express')
-var app = express()
+const exec = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+var app = express();
+
+var config = {
+  'applicationFqdn':       process.env.APPLICATION_FQDN,
+  'letsEncryptEmail':     process.env.LETS_ENCRYPT_EMAIL,
+  'letsEncryptServer':    process.env.LETS_ENCRYPT_SERVER
+}
+
+function config_complete(config) {
+  var keys = Object.keys(config);
+  for(var i = 0; i < keys.length; i++) {
+    if(typeof(config[keys[i]]) == 'undefined') {
+      return false;
+    }
+  }
+  return true;
+}
+
+function generate_certificate(callback) {
+  var cwd = __dirname;
+  var webroot = path.join(cwd, 'www');
+  var configdir = path.join(cwd, 'config');
+
+  var options = {
+    'cwd': path.join(__dirname, 'node_modules')
+  }
+  exec('node bin/letsencrypt.js --agree-tos --email ' + config['letsEncryptEmail'] + 
+            ' --webroot --webroot-path ' + webroot +
+            ' --config-dir' + configdir + 
+            ' --domains ' + env['applicationFqdn'] + 
+            ' --server ' + env['letsEncryptServer'],
+      options,
+      (error, stdout, stderr) => {
+        if(error) {
+          console.log(`exec error: ${error}`);
+          callback(true);
+        } else {
+          callback(false, { 'cert': cert, '{key}': key});
+          var cert = fs.readFileSync(path.join(configdir, 'live', env['applicationFqdn'], 'cert.pem')); 
+          var key = fs.readFileSync(path.join(configdir, 'live', env['applicationFqdn'], 'cert.pem'));
+          callback(false, { 'cert': cert, '{key}': key});
+        }
+      }
+  );
+}
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Nothing here.')
+});
+app.get('/generate_cert', function (request, response) {
+  generate_certificate(function(error, data) {
+    if(error) {
+      response.statusCode(503).end();
+    } else {
+      response.set({'Content-Type': 'application/json'});
+      resposne.write(JSON.stringify(data));
+      response.statusCode(201).end();
+    }
+  });
+});
+app.use(express.static(path.join(__dirname, 'www')));
 
-app.use(express.static(path.join(__dirname, 'www')))
-
-app.listen(5000, function () {
-  console.log('Listening on port 5000');
-})
+if(config_complete(config)) {
+  app.listen(5000, function () {
+    console.log('Listening on port 5000');
+  });
+} else {
+  console.log('Not all environment variables set.')
+  process.exit(1)
+}
